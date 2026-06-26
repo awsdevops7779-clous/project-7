31af8160d4d64c6336aa3100d99fffc783264205
AWS_Devops <awsdevops7779@gmail.com>
2026-06-26 15:20:49 +0530

    Update home route response message

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def home():
-    return "DevOps CI/CD Project Running Successfully"
+    return "DevOps CI/CD Project Running Successfully done"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
